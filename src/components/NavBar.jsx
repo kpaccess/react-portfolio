4c74340b9b2cd84bf102692f9784ef2f6aba8997
Krishna Pradhan <kpaccess@gmail.com>
2023-03-06 14:05:12 -0500

    added react-scroll npm

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-scroll";
 
 const NavBar = () => {
   const [nav, setNav] = useState(false);
@@ -36,7 +37,9 @@ const NavBar = () => {
             className="px-4 cursor-pointer capitalize text-gray-500 hover:scale-105 duration-200 font-medium"
             key={id}
           >
-            {link}
+            <Link to={link} smooth duration={500}>
+              {link}
+            </Link>
           </li>
         ))}
       </ul>
