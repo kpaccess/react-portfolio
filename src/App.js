7927e9b8e02a588e6f768bf2219023ac2322b8a0
Krishna Pradhan <kpaccess@gmail.com>
2023-03-06 11:11:27 -0500

    about page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import About from "./components/About";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import SocialLinks from "./components/SocialLinks";
@@ -6,7 +7,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Home />
-
+      <About />
       <SocialLinks />
     </div>
   );
