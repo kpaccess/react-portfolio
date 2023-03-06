aea95ab0d59c24014be172839c0d50e4856ba781
Krishna Pradhan <kpaccess@gmail.com>
2023-03-06 10:00:03 -0500

    added hero image and updated home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <h1 className="text-3xl font-bold underline font-signature">
-        Hello world!
-      </h1>
+      <Home />
     </div>
   );
 }
